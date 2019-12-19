bdf6b9b5fa28e591881c8289b8007fc49efd8ad0
sadakchap <aliceprerna@gmail.com>
2019-12-19 20:00:37 +0530

    changed html title

--- try.js
+++
@@ -1,4 +0,0 @@
-const tryElement = document.getElementById('try');
-const tryText = tryElement.innerText;
-tryElement.innerText = 'bye Idiot!';
-console.log({'before':tryText, 'after':tryElement.innerText});
